50ab372d4cb373580b7a3103d27f7a751623f711
이성수 <sslee@wylie.co.kr>
2025-10-31 11:10:20 +0900

    tailwind 셋팅 수정

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [
-    tailwindcss(), // 꼭 tailwindcss()로 호출
-    autoprefixer(),
-  ],
-};
