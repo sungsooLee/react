7c04b7c50aa188757bc86a871845734430d637eb
이성수 <sslee@wylie.co.kr>
2025-10-31 13:30:00 +0900

    라우터 샘플 추가

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,23 +1,23 @@
 // src/components/layout/Header/index.tsx
-import React from 'react'
-import HeaderType1 from './HeaderType1'
-import HeaderType2 from './HeaderType2'
-import type { HeaderType } from '../Layout'
+import React from "react";
+import HeaderType1 from "./HeaderType1";
+import HeaderType2 from "./HeaderType2";
+import type { HeaderType } from "../Layout";
 
 interface HeaderProps {
-  type: HeaderType
-  className?: string
+  type: HeaderType;
+  className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ type , className }) => {
+const Header: React.FC<HeaderProps> = ({ type, className }) => {
   switch (type) {
-    case 'type1':
-      return <HeaderType1 className={className} />
-    case 'type2':
-      return <HeaderType2 className={className} />
+    case "type1":
+      return <HeaderType1 className={className} />;
+    case "type2":
+      return <HeaderType2 className={className} />;
     default:
-      return null
+      return null;
   }
-}
+};
 
-export default Header
+export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,23 @@
-import Layout from '../components/layout/Layout'
-import cn from 'classnames'
-import styles from './Home.module.scss'
+import Layout from "../components/layout/Layout";
+import cn from "classnames";
+import styles from "./Home.module.scss";
+import { Link, useNavigate } from "react-router-dom";
 
-const Home = () => (
+const Home = () => {
+  const navigate = useNavigate(); // 훅 호출
+
+  return (
     <Layout headerType="type1" footerType="type1">
       <div className={cn(styles.home)}>
         <h2>Home Page</h2>
         <p>Home 콘텐츠입니다.</p>
+        <div className={styles.router_wrap}>
+          <Link to={"/"}>Index</Link> <br />
+          <button onClick={() => navigate("/")}>Index</button>
+        </div>
       </div>
     </Layout>
-);
+  );
+};
+
 export default Home;
-
-
