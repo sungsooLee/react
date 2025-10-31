2d819da510e75845fa65f19b0b0a6576f8a8eb57
이성수 <sslee@wylie.co.kr>
2025-10-31 09:49:02 +0900

    App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { useRoutes } from "react-router-dom";
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import routes from "virtual:generated-pages-react";
 import "./index.css";
 import ReactLogo from "./assets/icons/react.svg";
 
-function App() {
+function AppRoutes() {
   const element = useRoutes(routes);
-
-  // 라우트가 비어 있으면 기본 화면 보여주기
   return (
     element ?? (
       <div className="intro_wrap">
-        <img
-          src={ReactLogo}
-          alt="React Logo"
-          className="logo"
-        />
+        <img src={ReactLogo} alt="React Logo" className="logo" />
         <h1>Welcome to My React App</h1>
         <p>The current page is the index page</p>
       </div>
     )
+  );
+}
+
+function App() {
+  // 환경에 따라 basename 자동 전환
+  const basename = import.meta.env.MODE === "production" ? "/react" : "/";
+
+  return (
+    <BrowserRouter basename={basename}>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
