ff67d2ad773ad896801edec0cbe123ba88357b54
Larsene <lsstmi@naver.com>
2025-11-02 22:40:57 +0900

    토글 버튼 수정

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import cn from 'classnames'
-import styles from './ToggleButton.module.scss'
+import { useState } from "react";
+import cn from "classnames";
+import styles from "./ToggleButton.module.scss";
 
 interface ToggleButtonProps {
-  initial?: boolean
-  label?: string
-  onToggle?: (state: boolean) => void
-  className?: string
+  initial?: boolean;
+  label?: string;
+  onToggle?: (state: boolean) => void;
+  className?: string;
 }
 
-const ToggleButtonComponent = ({ label, initial = false, onToggle, className }: ToggleButtonProps) => {
-  const [toggled, setToggled] = useState(initial)
+export const ToggleButton = ({
+  initial = false,
+  label,
+  onToggle,
+  className,
+}: ToggleButtonProps) => {
+  const [toggled, setToggled] = useState(initial);
 
   const handleClick = () => {
-    const newState = !toggled
-    setToggled(newState)
-    if (onToggle) onToggle(newState)
-  }
+    const newState = !toggled;
+    setToggled(newState);
+    onToggle?.(newState);
+  };
 
   return (
     <button
-      type='button'
+      type="button"
       onClick={handleClick}
       className={cn(styles.btn, { [styles.active]: toggled }, className)}
     >
       {label}
     </button>
-  )
-}
-
-export const ToggleButton = ToggleButtonComponent
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Layout from "../components/layout/Layout";
 import cn from "classnames";
 import styles from "./Home.module.scss";
 import { Link, useNavigate } from "react-router-dom";
+import { ToggleButton } from "../components/common/ToggleButton";
 
 const Home = () => {
   const navigate = useNavigate(); // 훅 호출
+  const [label, setLabel] = useState("on");
 
   return (
     <Layout headerType="type1" footerType="type1">
@@ -14,6 +17,13 @@ const Home = () => {
         <div className={styles.router_wrap}>
           <Link to={"/"}>Index</Link> <br />
           <button onClick={() => navigate("/")}>Index</button>
+        </div>
+        <div>
+          <ToggleButton
+            initial={true}
+            label={label}
+            onToggle={(state) => setLabel(state ? "on" : "off")}
+          />
         </div>
       </div>
     </Layout>
