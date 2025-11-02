e024a7a356fe80e7b9dce4a3d420f02f6b5c4974
Larsene <lsstmi@naver.com>
2025-11-02 23:20:50 +0900

    토글 버튼 visible추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,11 @@ import { ToggleButton } from "../components/common/ToggleButton";
 const Home = () => {
   const navigate = useNavigate(); // 훅 호출
   const [label, setLabel] = useState("on");
+  const [visible, setVisible] = useState(true);
+  const handleClick = (state: boolean) => {
+    setLabel(state ? "on" : "off");
+    setVisible(state);
+  };
 
   return (
     <Layout headerType="type1" footerType="type1">
@@ -19,11 +24,8 @@ const Home = () => {
           <button onClick={() => navigate("/")}>Index</button>
         </div>
         <div>
-          <ToggleButton
-            initial={true}
-            label={label}
-            onToggle={(state) => setLabel(state ? "on" : "off")}
-          />
+          <ToggleButton initial={true} label={label} onToggle={handleClick} />
+          {visible && <div>visible</div>}
         </div>
       </div>
     </Layout>
